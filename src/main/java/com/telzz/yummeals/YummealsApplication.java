f3d99c7090e6dbf0d70c8105bacb3807163de92f
Usman <muhammad.uthman@palmpy-inc.com>
2025-06-13 14:48:16 +0100

    initial commit

---
+++ src/main/java/com/telzz/yummeals/YummealsApplication.java
@@ -0,0 +1,13 @@
+package com.telzz.yummeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YummealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YummealsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telzz/yummeals/YummealsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telzz.yummeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummealsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
